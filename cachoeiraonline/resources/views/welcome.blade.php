87fd34ee7a98d4a1322268c52bc01fee4fc3616c
MvZz <marcusvyny92@gmail.com>
2019-08-13 22:48:17 -0300

    coloquei a logo

--- cachoeiraonline/resources/views/welcome.blade.php
+++ cachoeiraonline/resources/views/welcome.blade.php
@@ -37,7 +37,7 @@
             <div class="content">
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div class="container">
-                        <a class ="navbar-brand" href='#'> Cachoeira Online </a> 
+                        <a class ="navbar-brand" href='#'> <img src="{{asset('img/logo.png')}}" alt="" srcset="" style="max-width: 100px; max-height: 60px;"> </a> 
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
